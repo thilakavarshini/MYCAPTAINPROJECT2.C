2b6717573a1c05774ee15c0f08b049312da8ace9
thilakavarshini <67496110+thilakavarshini@users.noreply.github.com>
2020-06-30 11:51:52 +0530

    Update Swapping numbers.c

--- Swapping numbers.c
+++ Swapping numbers.c
@@ -1,14 +1,16 @@
-#include <stdio.h> 
-int main() 
-{ 
-	int x = 20, y = 10; 
+#include <stdio.h>
 
-	// Code to swap 'x' and 'y' 
-	x = x + y; // x now becomes 30 
-	y = x - y; // y becomes 20  
-	x = x - y; // x becomes 10 
+int main()
+{
+    int a,b;
+    printf("enter the value of a and b");// a=2,b=3
+    scanf("%d%d",&a,&b);
+    printf("\n before swapping the value of a and b is %d %d :",a,b );// it will print the original value of a and b 2 ,3
+    a=a+b;// it gives 5
+    b=a-b;// it gives 2
+    a=a-b;// it gives 3
+    printf("\n after swapping the value of a and b is %d %d :",a,b);// now our output of a and b is 3,2
 
-	printf("After Swapping: x = %d, y = %d", x, y); 
+    return 0;
+}
 
-	return 0; 
-} 
